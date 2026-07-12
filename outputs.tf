6c64c1ad005c3aa5eb15d89a069b387dd48f6fbe
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "cosmosdb_sql_role_assignments" {
-  description = "All cosmosdb_sql_role_assignment resources"
-  value       = azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments
-}
 output "cosmosdb_sql_role_assignments_account_name" {
-  description = "List of account_name values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.account_name]
+  description = "Map of account_name values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.account_name }
 }
 output "cosmosdb_sql_role_assignments_name" {
-  description = "List of name values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.name]
+  description = "Map of name values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.name }
 }
 output "cosmosdb_sql_role_assignments_principal_id" {
-  description = "List of principal_id values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.principal_id]
+  description = "Map of principal_id values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.principal_id }
 }
 output "cosmosdb_sql_role_assignments_resource_group_name" {
-  description = "List of resource_group_name values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.resource_group_name]
+  description = "Map of resource_group_name values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.resource_group_name }
 }
 output "cosmosdb_sql_role_assignments_role_definition_id" {
-  description = "List of role_definition_id values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.role_definition_id]
+  description = "Map of role_definition_id values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.role_definition_id }
 }
 output "cosmosdb_sql_role_assignments_scope" {
-  description = "List of scope values across all cosmosdb_sql_role_assignments"
-  value       = [for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : v.scope]
+  description = "Map of scope values across all cosmosdb_sql_role_assignments, keyed the same as var.cosmosdb_sql_role_assignments"
+  value       = { for k, v in azurerm_cosmosdb_sql_role_assignment.cosmosdb_sql_role_assignments : k => v.scope }
 }
 
